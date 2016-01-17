548593b97dd9abaad3151f7edf0d4c0c48292fd7
Júlia Hentz <julia.hentz@gmail.com>
2016-01-17 12:59:13 +0100

    Established routing for EJS pages in server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var database = require('./database');
 
 var app = express();
 
 function initialise(){
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
 
     database.openDatabase(function(){
 
@@ -30,11 +34,22 @@ function server(){
 function router(){
 
     app.set('views', __dirname + '/public/views');
+    app.engine('ejs', require('ejs').renderFile);
     app.set('view engine', 'ejs');
 
     app.get('/', function(req, res) {
         res.render('pages/index');
     });
+    app.get('/projects', function(req,res){
+        res.render('pages/projects');
+    });
+    app.get('/about', function(req,res){
+        res.render('pages/about');
+    });
+    app.get('/contact', function(req,res){
+        res.render('pages/contact');
+    });
+
     app.use('/cms', express.static('cms'));
 
 };
